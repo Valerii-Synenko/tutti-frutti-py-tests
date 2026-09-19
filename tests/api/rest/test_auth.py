f1f4db94cd6c056186b7883510cdb140b2c62184
valerii synenko <valerii.synenko@gmail.com>
2026-09-19 16:34:53 +0200

    reactor: Move tests to AuthTest class

--- tests/api/rest/test_auth.py
+++ tests/api/rest/test_auth.py
@@ -6,10 +6,19 @@ from playwright.sync_api import APIResponse
 load_dotenv()
 
 
-def test_user_login2(auth_api_client):
-    user_login = os.getenv("ADMIN_EMAIL")
-    user_password = os.getenv("ADMIN_PASSWORD")
+class AuthTest:
+    """
+    Test for user authentication functionality.
 
-    response: APIResponse = auth_api_client.user_login(user_login, user_password)
+    Covers endpoints:
+    -------
+    `register`, `login`, `me`, `refresh`
+    """
 
-    assert response.status == 200, f"Expected status code 200, but got {response.status}"
+    def test_user_login(self, auth_api_client):
+        user_login = os.getenv("ADMIN_EMAIL")
+        user_password = os.getenv("ADMIN_PASSWORD")
+
+        response: APIResponse = auth_api_client.user_login(user_login, user_password)
+
+        assert response.status == 200, f"Expected status code 200, but got {response.status}"
