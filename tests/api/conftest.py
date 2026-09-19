e209ad82dcdc7384d3dc1ecc560f9d6798585d64
valerii synenko <valerii.synenko@gmail.com>
2026-09-19 16:35:35 +0200

    feat: Add api_context and auth_api_client fixtures

--- tests/api/conftest.py
+++ tests/api/conftest.py
@@ -1,11 +1,7 @@
-from collections.abc import Generator
-
 import pytest
-from playwright.sync_api import APIRequestContext, Playwright
+from framework.clients.rest.auth_client import AuthClient
 
 
 @pytest.fixture
-def api_context(playwright: Playwright) -> Generator[APIRequestContext]:
-    request_context = playwright.request.new_context()
-    yield request_context
-    request_context.dispose()
+def auth_api_client(api_context):
+    return AuthClient(api_context)
