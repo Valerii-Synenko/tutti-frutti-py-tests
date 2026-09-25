adf1c4bce11b8ad264ec2233b264d5151b02edc4
valerii synenko <valerii.synenko@gmail.com>
2026-09-26 00:06:20 +0200

    feat: Add aggregator class for DBs

---
+++ framework/clients/db/postgress/postgres_client.py
@@ -0,0 +1,13 @@
+from psycopg_pool import ConnectionPool
+
+
+class BasePostgresClient:
+    """
+    The base client for all Postgres databases.
+    """
+
+    def __init__(self, host: str, *, min_size: int = 1, max_size: int = 5):
+        self._pool = ConnectionPool(host, min_size=min_size, max_size=max_size)
+
+    def close_connection(self) -> None:
+        self._pool.close()
